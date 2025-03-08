923f920d18a97aa8d7fc1fe0c4e70156ef037d26
Omid Afroozeh <afroozeh.omid@gmail.com>
2025-03-08 11:39:04 +0100

    Unified String Dictionary

--- src/include/duckdb/common/enums/physical_operator_type.hpp
+++ src/include/duckdb/common/enums/physical_operator_type.hpp
@@ -118,7 +118,7 @@ enum class PhysicalOperatorType : uint8_t {
 	EXTENSION,
 	VERIFY_VECTOR,
 	UPDATE_EXTENSIONS,
-
+	UNIFIED_STRINGS,
 	// -----------------------------
 	// Secret
 	// -----------------------------
