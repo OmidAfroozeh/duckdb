145e2a9d12fe67d1c2a6af7c5810234ed3a0c766
Omid Afroozeh <afroozeh.omid@gmail.com>
2025-03-10 10:44:23 +0100

    [WIP] semi-working version

--- src/optimizer/UnifiedStringDictionary.cpp
+++ src/optimizer/UnifiedStringDictionary.cpp
@@ -2,7 +2,8 @@
 
 #include <cstring>
 #include <algorithm>
-
+#include <iostream>
+#include <string>
 namespace duckdb{
 
 uint64_t UnifiedStringsDictionary::USSR_prefix = 3;
@@ -28,12 +29,15 @@ UnifiedStringsDictionary::UnifiedStringsDictionary() {
 		HT_address = cast_uint64_to_pointer(USSR_prefix) + USSR_SIZE * USSR_SLOT_SIZE;
 	}
 
-
+//	memset(HT_address, '\0', HT_SIZE * HT_BUCKET_SIZE);
 	LinearProbingHT = make_uniq<LinearProbingHashTable>(HT_address);
 
 }
 
 UnifiedStringsDictionary *UnifiedStringsDictionary::getInstance() {
+	Printer printer;
+
+//	printer.Print("TESTTTT");
 	if(ussr_instance == nullptr){
 		lock_guard<std::mutex> guard(singletonLock);
 		if(ussr_instance == nullptr){
@@ -50,6 +54,14 @@ UnifiedStringsDictionary *UnifiedStringsDictionary::getInstance() {
 string_t UnifiedStringsDictionary::insert(const char * str, uint32_t len) {
 	hash_t h = Hash(str);
 	uint32_t hashPrefix = Load<uint32_t>(const_data_ptr_cast(&h));
+
+	auto lookup_res = LinearProbingHT.get()->lookup(hashPrefix);
+	if(lookup_res.IsValid()){
+		auto slot = lookup_res.GetIndex();
+		auto slot_ptr = DictionarySlot + slot;
+		return string_t(const_char_ptr_cast<uint64_t>(slot_ptr), len);
+	}
+
 	auto res = LinearProbingHT.get()->insert(hashPrefix, len);
 	if (res.IsValid()){
 		auto slot = res.GetIndex();
@@ -68,21 +80,28 @@ LinearProbingHashTable::LinearProbingHashTable(data_ptr_t bufferHT){
 	HT_atomic = reinterpret_cast<atomic<uint32_t>*>(bufferHT);
 	HT = reinterpret_cast<uint32_t *>(bufferHT);
 	currentEmptySlot = 1;
+	nFullBuckets = 0;
+//	nRejection_LongChain = 0;
+//	nRejections_SizeFull = 0;
+//	candidates = 0;
+//	accepted =0;
 }
 
 
 
 optional_idx LinearProbingHashTable::insert(uint32_t hashPrefix, uint32_t len) {
+
+//	candidates++;
+
 	uint16_t slot;
 	memcpy(&slot, &hashPrefix, 1U);
 
 	uint16_t hashExtract = hashPrefix >> 16;
-//	memcpy(&hashExtract, &(hashPrefix >> 16), 1U);
 
 
 	for (idx_t i = 0; i < PROBING_LIMIT; ++i) {
 		// loop around
-		uint32_t bucket = Load<uint32_t>(const_data_ptr_cast(HT + (slot + i % USSR_SIZE)));
+		uint32_t bucket = Load<uint32_t>(const_data_ptr_cast(HT + ((slot + i) % USSR_SIZE)));
 		uint16_t bucket_hashExtract = bucket >> 16;
 
 		if(bucket_hashExtract == hashExtract){
@@ -91,6 +110,7 @@ optional_idx LinearProbingHashTable::insert(uint32_t hashPrefix, uint32_t len) {
 
 		if(bucket == 0){
 			if(currentEmptySlot > USSR_SIZE){
+//				nRejections_SizeFull++;
 				return optional_idx();
 			}
 			uint32_t expected = 0;
@@ -98,8 +118,9 @@ optional_idx LinearProbingHashTable::insert(uint32_t hashPrefix, uint32_t len) {
 			desired = desired << 16;
 			desired |= UnsafeNumericCast<uint32_t>(currentEmptySlot);
 
-			if((HT_atomic + i)->compare_exchange_strong(expected, desired)){
-
+			if((HT_atomic + ((slot + i) % USSR_SIZE))->compare_exchange_strong(expected, desired)){
+//				accepted++;
+				nFullBuckets++;
 				auto ret = currentEmptySlot;
 				// 1 slot for the pre-computed hash,
 				currentEmptySlot += (len % 8 == 0) ? 1 + len / 8 : 2 + len / 8;
@@ -108,7 +129,35 @@ optional_idx LinearProbingHashTable::insert(uint32_t hashPrefix, uint32_t len) {
 			}
 		}
 	}
+//	nRejection_LongChain++;
 	return optional_idx();
 }
+
+optional_idx LinearProbingHashTable::lookup(uint32_t hashPrefix) {
+	uint16_t slot;
+	memcpy(&slot, &hashPrefix, 1U);
+
+	uint16_t hashExtract = hashPrefix >> 16;
+
+	for (idx_t i = 0; i < PROBING_LIMIT; ++i) {
+
+		uint32_t bucket = Load<uint32_t>(const_data_ptr_cast(HT + ((slot + i) % USSR_SIZE)));
+		uint16_t bucket_hashExtract = bucket >> 16;
+
+		if (bucket_hashExtract == hashExtract) {
+			return bucket & 0x0000FFFF;
+		}
+
+		return optional_idx();
+	}
+}
+
+void LinearProbingHashTable::getStatistics() {
+//	Printer::Print()
+
+}
+
+
+
 
 }// namespace duckdb
