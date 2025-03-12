f790be0f6f9b67c27c1584c8af82d61240295d08
Omid Afroozeh <afroozeh.omid@gmail.com>
2025-03-12 11:24:42 +0100

    fix formatting

--- src/common/types/hash.cpp
+++ src/common/types/hash.cpp
@@ -8,14 +8,10 @@
 
 #include "duckdb/optimizer/UnifiedStringDictionary.h"
 
-
 #include <functional>
 #include <cmath>
 
 namespace duckdb {
-
-
-
 
 template <>
 hash_t Hash(uint64_t val) {
@@ -115,9 +111,8 @@ hash_t Hash(string_t val) {
 		D_ASSERT(h == Hash(val.GetData(), val.GetSize()));
 
 		return h;
-	}
-	else if((USSR_MASK & cast_pointer_to_uint64(val.GetPointer())) == UnifiedStringsDictionary::USSR_prefix){
-		return *(reinterpret_cast<uint64_t *>(val.GetPointer()) -1);
+	} else if ((USSR_MASK & cast_pointer_to_uint64(val.GetPointer())) == UnifiedStringsDictionary::USSR_prefix) {
+		return *(reinterpret_cast<uint64_t *>(val.GetPointer()) - 1);
 	}
 	return Hash(val.GetData(), val.GetSize());
 }
