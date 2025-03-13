ea3b835185779ea7c4761eae03fc87256aa573d4
Omid Afroozeh <afroozeh.omid@gmail.com>
2025-03-13 16:46:00 +0100

    Added tracking statistics in DEBUG mode + fine granular locking

--- src/main/client_context.cpp
+++ src/main/client_context.cpp
@@ -44,6 +44,9 @@
 #include "duckdb/transaction/transaction_context.hpp"
 #include "duckdb/transaction/transaction_manager.hpp"
 
+#include "duckdb/optimizer/UnifiedStringDictionary.h"
+
+
 namespace duckdb {
 
 struct ActiveQueryContext {
@@ -224,7 +227,7 @@ void ClientContext::BeginQueryInternal(ClientContextLock &lock, const string &qu
 ErrorData ClientContext::EndQueryInternal(ClientContextLock &lock, bool success, bool invalidate_transaction,
                                           optional_ptr<ErrorData> previous_error) {
 	client_data->profiler->EndQuery();
-
+	UnifiedStringsDictionary::destroy_UnifiedStrings();
 	if (active_query->executor) {
 		active_query->executor->CancelTasks();
 	}
@@ -271,6 +274,7 @@ ErrorData ClientContext::EndQueryInternal(ClientContextLock &lock, bool success,
 			s->QueryEnd(*this, previous_error);
 		}
 	}
+
 
 	return error;
 }
