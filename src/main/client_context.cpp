5788eb339f4bd3d5b410a9e9c2f100ff56dcc685
Omid Afroozeh <afroozeh.omid@gmail.com>
2025-03-14 14:23:27 +0100

    trying to fix the seg fault

--- src/main/client_context.cpp
+++ src/main/client_context.cpp
@@ -45,8 +45,6 @@
 #include "duckdb/transaction/transaction_manager.hpp"
 #include "duckdb/logging/log_type.hpp"
 
-#include "duckdb/optimizer/UnifiedStringDictionary.h"
-
 namespace duckdb {
 
 struct ActiveQueryContext {
@@ -227,7 +225,7 @@ void ClientContext::BeginQueryInternal(ClientContextLock &lock, const string &qu
 ErrorData ClientContext::EndQueryInternal(ClientContextLock &lock, bool success, bool invalidate_transaction,
                                           optional_ptr<ErrorData> previous_error) {
 	client_data->profiler->EndQuery();
-	//	UnifiedStringsDictionary::destroy_UnifiedStrings();
+//		UnifiedStringsDictionary::destroy_UnifiedStrings();
 	if (active_query->executor) {
 		active_query->executor->CancelTasks();
 	}
