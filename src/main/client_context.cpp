131ceaf68d579989f34a4215d2bc4b46f174cdba
Omid Afroozeh <afroozeh.omid@gmail.com>
2025-03-14 11:38:11 +0100

    format fix + removing unused variable

--- src/main/client_context.cpp
+++ src/main/client_context.cpp
@@ -227,7 +227,7 @@ void ClientContext::BeginQueryInternal(ClientContextLock &lock, const string &qu
 ErrorData ClientContext::EndQueryInternal(ClientContextLock &lock, bool success, bool invalidate_transaction,
                                           optional_ptr<ErrorData> previous_error) {
 	client_data->profiler->EndQuery();
-	UnifiedStringsDictionary::destroy_UnifiedStrings();
+	//	UnifiedStringsDictionary::destroy_UnifiedStrings();
 	if (active_query->executor) {
 		active_query->executor->CancelTasks();
 	}
