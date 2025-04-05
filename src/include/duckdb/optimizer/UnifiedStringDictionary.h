9fe5c50e1262bcd8bd315b7516203eec26433db6
Omid Afroozeh <afroozeh.omid@gmail.com>
2025-04-05 11:44:47 +0200

    USSR api change

--- src/include/duckdb/optimizer/UnifiedStringDictionary.h
+++ src/include/duckdb/optimizer/UnifiedStringDictionary.h
@@ -11,44 +11,6 @@
 
 namespace duckdb {
 
-struct LinearProbingHashTable {
-private:
-	static constexpr uint64_t BUFFER_SIZE = static_cast<uint64_t>(1024 * 1024);
-
-	static constexpr uint64_t USSR_MASK = 0xFFFFFFFFFFF80000;
-
-	static constexpr uint64_t USSR_SLOT_SIZE = 8;
-	static constexpr uint64_t USSR_SIZE = 0xFFFF;
-
-	// first two bytes are the slot number into the data region
-	// and the second two bytes are the hash extract (a part of the original string's hash)
-	static constexpr uint64_t HT_BUCKET_SIZE = 4;
-	static constexpr uint64_t HT_SIZE = 0xFFFF;
-
-	static constexpr idx_t PROBING_LIMIT = 3;
-
-private:
-	uint64_t currentEmptySlot;
-
-	uint32_t *HT;
-
-	// every attempt on inserting a string
-	uint64_t candidates;
-	// accepted strings into the USSR
-	uint64_t accepted;
-
-	uint64_t nRejections_SizeFull;
-	uint64_t nRejections_Probing;
-
-public:
-	explicit LinearProbingHashTable(data_ptr_t bufferHT);
-
-	optional_idx insert(uint32_t hashPrefix, uint32_t len);
-	optional_idx lookup(uint32_t hashPrefix);
-
-	void getStatistics();
-};
-
 // Singleton
 class UnifiedStringsDictionary {
 public:
@@ -72,10 +34,23 @@ private:
 	// Start of the DataRegion
 	uint64_t *DataRegion;
 
-	unique_ptr<LinearProbingHashTable> LinearProbingHT;
-
 	// temporary solution for concurrency
 	std::mutex insertLock;
+
+	uint64_t currentEmptySlot;
+
+	uint32_t *HT;
+
+	// every attempt on inserting a string
+	uint64_t candidates;
+	// accepted strings into the USSR
+	uint64_t accepted;
+
+	uint64_t already_in;
+
+	uint64_t nRejections_SizeFull;
+	uint64_t nRejections_Probing;
+
 
 public:
 	UnifiedStringsDictionary();
@@ -83,6 +58,12 @@ public:
 	uint64_t USSR_prefix;
 
 	string_t insert(string_t str);
+
+private:
+	string_t insertInternal(string_t str);
+
+	void getStatistics();
+
 };
 
 } // namespace duckdb
