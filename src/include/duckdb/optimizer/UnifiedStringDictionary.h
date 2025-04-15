a49551cff61c42daea1f600656ccd1083a33c84b
Omid Afroozeh <afroozeh.omid@gmail.com>
2025-04-15 08:00:42 +0200

    relaxed_memory loads

--- src/include/duckdb/optimizer/UnifiedStringDictionary.h
+++ src/include/duckdb/optimizer/UnifiedStringDictionary.h
@@ -14,24 +14,24 @@ namespace duckdb {
 // Singleton
 class UnifiedStringsDictionary {
 public:
-	static constexpr uint64_t ATTEMPT_THRESHOLD = 100000;
+	static constexpr const uint64_t ATTEMPT_THRESHOLD = 100000;
 
-	static constexpr uint64_t MAX_STRING_LENGTH = 255;
+	static constexpr const uint64_t MAX_STRING_LENGTH = 255;
 
 
-	static constexpr uint64_t BUFFER_SIZE = static_cast<uint64_t>(1024 * 1024);
+	static constexpr const uint64_t BUFFER_SIZE = static_cast<uint64_t>(1024 * 1024);
 
-	static constexpr uint64_t USSR_MASK = 0xFFFFFFFFFFF80000;
+	static constexpr const uint64_t USSR_MASK = 0xFFFFFFFFFFF80000;
 
-	static constexpr uint64_t USSR_SLOT_SIZE = 8;
-	static constexpr uint64_t USSR_SIZE = 0xFFFF;
+	static constexpr const uint64_t USSR_SLOT_SIZE = 8;
+	static constexpr const uint64_t USSR_SIZE = 0xFFFF;
 
 	// first two bytes are the slot number into the data region
 	// and the second two bytes are the hash extract (a part of the original string's hash)
-	static constexpr uint64_t HT_BUCKET_SIZE = 4;
-	static constexpr uint64_t HT_SIZE = 0xFFFF;
+	static constexpr const uint64_t HT_BUCKET_SIZE = 4;
+	static constexpr const uint64_t HT_SIZE = 0xFFFF;
 
-	static constexpr idx_t PROBING_LIMIT = 3;
+	static constexpr const idx_t PROBING_LIMIT = 3;
 
 private:
 	// Overarching USSR buffer, contains DataRegion + HT + extra, 1MB size
@@ -64,7 +64,7 @@ public:
 	string_t insert(string_t str);
 
 private:
-	string_t insertInternal(string_t str, hash_t hash = 0);
+	string_t insertInternal(string_t str);
 
 	void getStatistics();
 };
