31ef391f5ec2ac1e52dbd78322d5bfbac71cab58
Omid Afroozeh <afroozeh.omid@gmail.com>
2025-05-12 15:56:13 +0200

    Revert "merged and removed context for compresseion interface"
    This reverts commit 83a0bf729f806b744a2b6594f2e6c83988fdbd01.

--- src/include/duckdb/storage/compression/chimp/chimp_scan.hpp
+++ src/include/duckdb/storage/compression/chimp/chimp_scan.hpp
@@ -252,7 +252,7 @@ public:
 };
 
 template <class T>
-unique_ptr<SegmentScanState> ChimpInitScan(ColumnSegment &segment) {
+unique_ptr<SegmentScanState> ChimpInitScan(ColumnSegment &segment, optional_ptr<ClientContext> context = nullptr) {
 	auto result = make_uniq_base<SegmentScanState, ChimpScanState<T>>(segment);
 	return result;
 }
