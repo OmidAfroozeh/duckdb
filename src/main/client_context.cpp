5dc3ab7572bd15b80aebe9063068791ce1401c06
Omid Afroozeh <afroozeh.omid@gmail.com>
2025-05-16 14:34:34 +0200

    testing cache misses, 32MB ussr size 4.0

--- src/main/client_context.cpp
+++ src/main/client_context.cpp
@@ -215,7 +215,7 @@ void ClientContext::BeginQueryInternal(ClientContextLock &lock, const string &qu
 	LogQueryInternal(lock, query);
 	active_query->query = query;
 
-	active_query->ussr = make_uniq<UnifiedStringsDictionary>(64ull);
+	active_query->ussr = make_uniq<UnifiedStringsDictionary>(2ull);
 
 	query_progress.Initialize();
 	// Notify any registered state of query begin
