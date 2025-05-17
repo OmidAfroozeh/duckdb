a4d525da86c157eef09ed4912ff12aa8fd16ba5f
Omid Afroozeh <afroozeh.omid@gmail.com>
2025-05-17 17:46:57 +0200

    more cleaning

--- src/execution/operator/helper/physical_ussr_insertion.cpp
+++ src/execution/operator/helper/physical_ussr_insertion.cpp
@@ -8,14 +8,18 @@ public:
 	explicit USSRInsertionState(ExecutionContext &context, idx_t cols) {
 		for (idx_t i = 0; i < cols; ++i) {
 			current_dict_ids.push_back("");
+			inserted.push_back({});
+			count.push_back({});
+			analysis_budget.push_back(0);
+			current_analysis_count.push_back(0);
 		}
 	}
 	unordered_set<string> strs;
-	vector<bool> inserted;
-	vector<idx_t> count;
+	vector<vector<bool>> inserted;
+	vector<vector<idx_t>> count;
 	vector<string> current_dict_ids;
-	idx_t analysis_budget;
-	idx_t current_analysis_count;
+	vector<idx_t> analysis_budget;
+	vector<idx_t> current_analysis_count;
 };
 
 class USSRInsertionGState : public GlobalOperatorState {
@@ -54,27 +58,28 @@ OperatorResultType PhysicalUnifiedString::Execute(ExecutionContext &context, Dat
 		} else {
 			if (insert_to_ussr[col_idx] &&
 			    DictionaryVector::DictionaryId(input.data[col_idx]) != state.current_dict_ids[col_idx]) {
+
 				// initialize the local state
-				state.inserted.clear();
-				state.count.clear();
-				state.inserted.reserve(size.GetIndex());
-				state.count.reserve(size.GetIndex());
+				state.inserted[col_idx].clear();
+				state.count[col_idx].clear();
+				state.inserted[col_idx].reserve(size.GetIndex());
+				state.count[col_idx].reserve(size.GetIndex());
 				for (idx_t i = 0; i < size.GetIndex(); i++) {
-					state.inserted.push_back(false);
-					state.count.push_back(0);
+					state.inserted[col_idx].push_back(false);
+					state.count[col_idx].push_back(0);
 				}
-				state.analysis_budget = 0;
-				state.current_analysis_count = 1;
+				state.analysis_budget[col_idx] = 1000000;
+				state.current_analysis_count[col_idx] = 1;
 				auto &sel = DictionaryVector::SelVector(input.data[col_idx]);
 				for (idx_t i = 0; i < input.size(); i++) {
-					state.count[sel.data()[i]]++;
+					state.count[col_idx][sel.data()[i]]++;
 				}
 
 				vector<idx_t> priority_selection;
-				for (idx_t i = 1; i < state.count.size(); i++) {
-					if (state.count[i] > (25 * state.current_analysis_count)) {
+				for (idx_t i = 1; i < state.count[col_idx].size(); i++) {
+					if (state.count[col_idx][i] > (2 * state.current_analysis_count[col_idx])) {
 						priority_selection.push_back(i);
-												state.inserted[i] = true;
+												state.inserted[col_idx][i] = true;
 					}
 				}
 
@@ -82,84 +87,28 @@ OperatorResultType PhysicalUnifiedString::Execute(ExecutionContext &context, Dat
 				USSR_insertion_loop(dict.GetData(), size.GetIndex(), context.client, priority_selection, true);
 			} else if (insert_to_ussr[col_idx] &&
 			           DictionaryVector::DictionaryId(input.data[col_idx]) == state.current_dict_ids[col_idx] &&
-			           state.current_analysis_count <= state.analysis_budget && input.size() == STANDARD_VECTOR_SIZE) {
-				state.current_analysis_count++;
+			           state.current_analysis_count[col_idx] <= state.analysis_budget[col_idx]) {
+				state.current_analysis_count[col_idx]++;
 				//				Printer::Print(to_string(++vec_counter));
 
 				auto &sel = DictionaryVector::SelVector(input.data[col_idx]);
 				for (idx_t i = 0; i < input.size(); i++) {
-					state.count[sel.data()[i]]++;
+					state.count[col_idx][sel.data()[i]]++;
 				}
 
 				vector<idx_t> priority_selection;
-				for (idx_t i = 1; i < state.count.size(); ++i) {
-					if (state.count[i] > (15 * state.current_analysis_count) && !state.inserted[i]) {
+				for (idx_t i = 1; i < state.count[col_idx].size(); ++i) {
+					if (state.count[col_idx][i] > (2 * state.current_analysis_count[col_idx]) && !state.inserted[col_idx][i]) {
 						priority_selection.push_back(i);
-						state.inserted[i] = true;
+						state.inserted[col_idx][i] = true;
 					}
 				}
 				USSR_insertion_loop(dict.GetData(), size.GetIndex(), context.client, priority_selection, true);
 			}
 		}
-		//
-		//
-		//		chunk.Reference(input);
-		//		return OperatorResultType::NEED_MORE_INPUT;
-		//	context.client.GetCurrentQueryUssr().insert();
 	}
-
-	//
-	//		if(insert_to_ussr[col_idx] && DictionaryVector::DictionaryId(input.data[col_idx]) !=
-	//state.current_dict_ids[col_idx]){
-	//
-	//
-	//
-	//
-	//
-	//			Printer::Print(to_string(size.GetIndex()));
-	//
-	//			// initialize the local state
-	//			state.inserted.reserve(size.GetIndex());
-	//			for (idx_t i = 0; i < size.GetIndex(); ++i) {
-	//				state.inserted.push_back(false);
-	//			}
-	//
-	//			state.current_dict_ids[col_idx] = DictionaryVector::DictionaryId(input.data[col_idx]);
-	////
-	////			std::vector<idx_t> insertion_priority;
-	////
-	////			{
-	////				std::lock_guard<std::mutex> lock(gstateussr.budget_lock);
-	////				if(gstateussr.analyzing_budget > 0){
-	////					gstateussr.analyzing_budget--;
-	////				}
-	////			}
-	////
-	////			if(gstateussr.analyzing_budget && size.GetIndex() > 1000){
-	////				std::vector<uint32_t> count(size.GetIndex());
-	////
-	////				for (idx_t i = 0; i < input.size(); ++i) {
-	////					count[(DictionaryVector::SelVector(input.data[col_idx]).data()[i])]++;
-	////				}
-	////				for (idx_t i = 1; i < count.size(); ++i) {
-	////					if(count[i] > (input.size() / count.size())){
-	////						insertion_priority.push_back(i);
-	////					}
-	////				}
-	////			}
-	//			USSR_insertion_loop(dict.GetData(), size.GetIndex(), context.client, {});
-	////			auto start = reinterpret_cast<string_t *>(dict.GetData());
-	////
-	////			for (idx_t i = 1; i < size.GetIndex() - 1; ++i) {
-	////
-	//				start[i] = context.client.GetCurrentQueryUssr().insert(start[i]);
-	//			}
-	//		}
-	//	}
-
 	chunk.Reference(input);
 	return OperatorResultType::NEED_MORE_INPUT;
-	//	context.client.GetCurrentQueryUssr().insert();
 }
 
 unique_ptr<OperatorState> PhysicalUnifiedString::GetOperatorState(ExecutionContext &context) const {
