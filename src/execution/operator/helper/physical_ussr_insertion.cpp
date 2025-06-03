afcf1f1c06446b67cb1660d9c4bfc9ead17ab598
Omid Afroozeh <afroozeh.omid@gmail.com>
2025-06-03 19:50:59 +0200

    trying to find the segfault

--- src/execution/operator/helper/physical_ussr_insertion.cpp
+++ src/execution/operator/helper/physical_ussr_insertion.cpp
@@ -45,77 +45,77 @@ OperatorResultType PhysicalUnifiedString::Execute(ExecutionContext &context, Dat
 		// no selection vector analysis
 		auto &dict = DictionaryVector::Child(input.data[col_idx]);
 		auto size = DictionaryVector::DictionarySize(input.data[col_idx]);
+		auto &dict_id = DictionaryVector::DictionaryId(input.data[col_idx]);
 		if (!size.IsValid()) {
 			continue;
 		}
 		auto dict_validity = FlatVector::Validity(dict);
 		Printer::Print(dict_validity.ToString());
 		bool isParquet;
-		if(DictionaryVector::DictionaryId(input.data[col_idx])[0] == 'X'){
+		if(DictionaryVector::DictionaryId(input.data[col_idx])[0] == 'x'){
 			isParquet = true;
 		} else{
 			isParquet = false;
 		}
 
-//		if (size.GetIndex() <= 500000000) {
-//			if (insert_to_ussr[col_idx] &&
-//			    DictionaryVector::DictionaryId(input.data[col_idx]) != state.current_dict_ids[col_idx]) {
-//				state.current_dict_ids[col_idx] = DictionaryVector::DictionaryId(input.data[col_idx]);
-//				USSR_insertion_loop(dict.GetData(), size.GetIndex(), context.client, {}, isParquet, false);
-//			}
-//		} else {
-//			if (insert_to_ussr[col_idx] &&
-//			    DictionaryVector::DictionaryId(input.data[col_idx]) != state.current_dict_ids[col_idx]) {
-//
-//				// initialize the local state
-//				state.inserted[col_idx].clear();
-//				state.count[col_idx].clear();
-//				state.inserted[col_idx].reserve(size.GetIndex());
-//				state.count[col_idx].reserve(size.GetIndex());
-//				for (idx_t i = 0; i < size.GetIndex(); i++) {
-//					state.inserted[col_idx].push_back(false);
-//					state.count[col_idx].push_back(0);
-//				}
-//				state.analysis_budget[col_idx] = 5;
-//				state.current_analysis_count[col_idx] = 1;
-//				auto &sel = DictionaryVector::SelVector(input.data[col_idx]);
-//				for (idx_t i = 0; i < input.size(); i++) {
-//					state.count[col_idx][sel.data()[i]]++;
-//				}
-//
-//				vector<idx_t> priority_selection;
-//				for (idx_t i = 1; i < state.count[col_idx].size(); i++) {
-//					if (state.count[col_idx][i] > (25 * state.current_analysis_count[col_idx])) {
-//						priority_selection.push_back(i);
-//						state.inserted[col_idx][i] = true;
-//					}
-//				}
-//
-//				state.current_dict_ids[col_idx] = DictionaryVector::DictionaryId(input.data[col_idx]);
-//				USSR_insertion_loop(dict.GetData(), size.GetIndex(), context.client, priority_selection, isParquet, true);
-//			} else if (insert_to_ussr[col_idx] &&
-//			           DictionaryVector::DictionaryId(input.data[col_idx]) == state.current_dict_ids[col_idx] &&
-//			           state.current_analysis_count[col_idx] <= state.analysis_budget[col_idx]) {
-//				state.current_analysis_count[col_idx]++;
-//				//				Printer::Print(to_string(++vec_counter));
-//
-//				auto &sel = DictionaryVector::SelVector(input.data[col_idx]);
-//				for (idx_t i = 0; i < input.size(); i++) {
-//					state.count[col_idx][sel.data()[i]]++;
-//				}
-//
-//				vector<idx_t> priority_selection;
-//				for (idx_t i = 1; i < state.count[col_idx].size(); ++i) {
-//					if (state.count[col_idx][i] > (27 * state.current_analysis_count[col_idx]) &&
-//					    !state.inserted[col_idx][i]) {
-//						priority_selection.push_back(i);
-//						state.inserted[col_idx][i] = true;
-//					}
-//				}
-//				USSR_insertion_loop(dict.GetData(), size.GetIndex(), context.client, priority_selection, isParquet, true);
-//			}
-//		}
-//	}
+		if (size.GetIndex() <= 500000000) {
+			if (insert_to_ussr[col_idx] &&
+			    DictionaryVector::DictionaryId(input.data[col_idx]) != state.current_dict_ids[col_idx]) {
+				state.current_dict_ids[col_idx] = DictionaryVector::DictionaryId(input.data[col_idx]);
+				USSR_insertion_loop(dict.GetData(), size.GetIndex(), context.client, {}, isParquet, false);
+			}
+		} else {
+			if (insert_to_ussr[col_idx] &&
+			    DictionaryVector::DictionaryId(input.data[col_idx]) != state.current_dict_ids[col_idx]) {
+
+				// initialize the local state
+				state.inserted[col_idx].clear();
+				state.count[col_idx].clear();
+				state.inserted[col_idx].reserve(size.GetIndex());
+				state.count[col_idx].reserve(size.GetIndex());
+				for (idx_t i = 0; i < size.GetIndex(); i++) {
+					state.inserted[col_idx].push_back(false);
+					state.count[col_idx].push_back(0);
+				}
+				state.analysis_budget[col_idx] = 5;
+				state.current_analysis_count[col_idx] = 1;
+				auto &sel = DictionaryVector::SelVector(input.data[col_idx]);
+				for (idx_t i = 0; i < input.size(); i++) {
+					state.count[col_idx][sel.data()[i]]++;
+				}
+
+				vector<idx_t> priority_selection;
+				for (idx_t i = 1; i < state.count[col_idx].size(); i++) {
+					if (state.count[col_idx][i] > (25 * state.current_analysis_count[col_idx])) {
+						priority_selection.push_back(i);
+						state.inserted[col_idx][i] = true;
+					}
+				}
+
+				state.current_dict_ids[col_idx] = DictionaryVector::DictionaryId(input.data[col_idx]);
+				USSR_insertion_loop(dict.GetData(), size.GetIndex(), context.client, priority_selection, isParquet, true);
+			} else if (insert_to_ussr[col_idx] &&
+			           DictionaryVector::DictionaryId(input.data[col_idx]) == state.current_dict_ids[col_idx] &&
+			           state.current_analysis_count[col_idx] <= state.analysis_budget[col_idx]) {
+				state.current_analysis_count[col_idx]++;
+				//				Printer::Print(to_string(++vec_counter));
+
+				auto &sel = DictionaryVector::SelVector(input.data[col_idx]);
+				for (idx_t i = 0; i < input.size(); i++) {
+					state.count[col_idx][sel.data()[i]]++;
+				}
+
+				vector<idx_t> priority_selection;
+				for (idx_t i = 1; i < state.count[col_idx].size(); ++i) {
+					if (state.count[col_idx][i] > (27 * state.current_analysis_count[col_idx]) &&
+					    !state.inserted[col_idx][i]) {
+						priority_selection.push_back(i);
+						state.inserted[col_idx][i] = true;
+					}
+				}
+				USSR_insertion_loop(dict.GetData(), size.GetIndex(), context.client, priority_selection, isParquet, true);
+			}
+		}
 	}
 	chunk.Reference(input);
 	return OperatorResultType::NEED_MORE_INPUT;
